f1ed5212cb2144bb59fe1b02a15934f431a3fcb0
Dave Carson <dave@onthefencedevelopment.com>
2018-12-08 17:19:38 +0000

    Initial Menu configuration

--- PersonalEncrypter.UWP/MainPage.xaml.cs
+++ PersonalEncrypter.UWP/MainPage.xaml.cs
@@ -3,11 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.Storage;
-using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,42 +22,9 @@ namespace PersonalEncrypter.UWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private StorageFolder keyFolder;
-
         public MainPage()
         {
             this.InitializeComponent();
-
-            btnGenerate.Click += new RoutedEventHandler(btnGenerate_Click);
-            btnBrowse.Click += new RoutedEventHandler(btnBrowse_Click);
-        }
-
-        private async void btnBrowse_Click(object sender, RoutedEventArgs e)
-        {
-            var foo = new FolderPicker();
-            foo.ViewMode = PickerViewMode.List;
-            foo.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
-            foo.FileTypeFilter.Add("*");
-
-            keyFolder = await foo.PickSingleFolderAsync();
-        }
-        private async void btnGenerate_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                var encrypter = new SodiumEncypter.Encrypter();
-                var keyPair = encrypter.GenerateKeyPair();
-
-                var prKey = await keyFolder.CreateFileAsync("DavePrivateKey.txt", CreationCollisionOption.ReplaceExisting);
-                await FileIO.WriteTextAsync(prKey, Convert.ToBase64String(keyPair.PrivateKey));
-
-                //File.WriteAllText(Path.Combine(keyFolder.Path, "DavePrivateKey.txt"), Convert.ToBase64String(keyPair.PrivateKey));
-            }
-            catch (Exception ex)
-            {
-
-                throw;
-            }
         }
     }
 }
