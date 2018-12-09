ff08072a9e9d6c1cb27b5f8af8ff9a485d271b84
Dave Carson <dave@onthefencedevelopment.com>
2018-12-09 20:39:38 +0000

    Refactoring

--- PersonalEncrypter.Shared/Interfaces/IEncrypter.cs
+++ PersonalEncrypter.Shared/Interfaces/IEncrypter.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PersonalEncrypter.Shared.Interfaces
+﻿namespace PersonalEncrypter.Shared.Interfaces
 {
     public interface IEncrypter
     {
